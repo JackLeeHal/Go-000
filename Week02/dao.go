725793316183629e2a969eb23c521a396e811aee
Jack Li <hao.li@anz.com>
2020-12-02 17:47:52 +0800

    add test

--- Week02/dao.go
+++ Week02/dao.go
@@ -1,24 +1,45 @@
 package Week02
 
-import "errors"
-
-var (
-	errCommentsNotFound = errors.New("comments not found")
-	errBalanceNotFound  = errors.New("balance not found")
+import (
+	"errors"
+	"fmt"
 )
 
-func GetComments(id string, db map[string][]Comment) ([]Comment, error) {
-	comments := db[id]
+var (
+	ErrNoRows  = errors.New("no rows found")
+	commentsDB = map[string][]Comment{
+		"test": {},
+	}
+	balanceDB = map[string]*Balance{}
+)
+
+type CustomError struct {
+	name  string
+	cause error
+}
+
+func (e *CustomError) Error() string {
+	return fmt.Sprint("error ", e.name)
+}
+
+func GetComments(id string) ([]Comment, error) {
+	comments := commentsDB[id]
 	if len(comments) == 0 {
-		return nil, errCommentsNotFound
+		return nil, &CustomError{
+			name:  "Comments",
+			cause: ErrNoRows,
+		}
 	}
 	return comments, nil
 }
 
-func GetBalance(id string, db map[string]*Balance) (*Balance, error) {
-	balance := db[id]
+func GetBalance(id string) (*Balance, error) {
+	balance := balanceDB[id]
 	if balance == nil {
-		return nil, errBalanceNotFound
+		return nil, &CustomError{
+			name:  "Balance",
+			cause: ErrNoRows,
+		}
 	}
 	return balance, nil
 }
---
+++ Week02/service.go
@@ -0,0 +1,25 @@
+package Week02
+
+import "errors"
+
+func CommentsService(id string) ([]Comment, error) {
+	c, err := GetComments(id)
+	if err != nil {
+		// log
+		var e *CustomError
+		if errors.As(err, &e) {
+			return []Comment{}, nil
+		}
+		return nil, err
+	}
+	return c, nil
+}
+
+func BalanceService(id string) (*Balance, error) {
+	b, err := GetBalance(id)
+	if err != nil {
+		// log
+		return nil, err
+	}
+	return b, nil
+}
